e371f595d4afcee03ee9abdaec4a575610fd2378
zack <zackaryfotheringham@hotmail.com>
2023-01-14 10:58:11 -0500

    add components and styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import Header from "./components/Header";
+import Experience from "./components/ExpSection";
+import Education from "./components/EduSection";
+class App extends React.Component {
+  render() {
+    return (
+      <>        
+        <div className="cv-container bg-slate-50 grid">
+          <div className="cv-content grid my-6 p-4 bg-slate-200 rounded-lg shadow-xl w-11/12 place-self-center">
+            <Header />
+            <Experience />
+            <div className="border-b-2 rounded-full border-black border-opacity-10 w-28 place-self-center mb-2"></div>
+            <Education />
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
