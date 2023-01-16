bc45ec8b53c0a88e3d26eadc3032530ff9ed9df3
Zack F <zackaryfotheringham@hotmail.com>
2023-01-16 09:33:07 -0500

    remove unused params

--- src/components/EduSection.jsx
+++ src/components/EduSection.jsx
@@ -14,7 +14,7 @@ class EduSection extends React.Component{
         <section className="mt-2 grid">
           <h2 className="place-self-center text-xl text-blue-400 mb-2">Education</h2>
           {
-            Object.entries(this.state.initialData).map(degree => <DegreeCard degree={degree[1].degree} study={degree[1].study} school={degree[1].school} dateStart={degree[1].dateStart} dateEnd={degree[1].dateEnd} key={degree[0]} objectKey={degree[0]}/>)
+            Object.entries(this.state.initialData).map(degree => <DegreeCard key={degree[0]} objectKey={degree[0]}/>)
           }
           <button className="hidden">Add Education</button>
         </section>
